b3c2e9d2449b659a76e17fcb48fa285c7e9c08c4
Olaff Molero <olaffmartin@gmail.com>
2017-09-28 09:11:05 -0500

    first commit

---
+++ src/main/java/com/example/demo/Spring5RecipeApp1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipeApp1Application.class, args);
+	}
+}
